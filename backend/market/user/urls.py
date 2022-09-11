0149aa9c849946f1f31831d9b27da98a0d8a40b3
arshharkial <theadsh99@gmail.com>
2022-09-11 13:34:44 +0530

    Added URLs; formatting

--- backend/market/user/urls.py
+++ backend/market/user/urls.py
@@ -1,5 +1,7 @@
 from django.urls.conf import include
 from .views import (
+    AddressDetail,
+    AddressList,
     RegisterAPI,
     LoginAPI,
     UserAPI,
@@ -13,15 +15,67 @@ from django.urls import path
 from knox import views as knox_views
 
 urlpatterns = [
-    path("register/", RegisterAPI.as_view(), name="register"),
-    path("login/", LoginAPI.as_view(), name="login"),
-    path("logout/", knox_views.LogoutView.as_view(), name="logout"),
-    path("logout-all/", knox_views.LogoutAllView.as_view(), name="logoutall"),
-    path("user/", UserAPI.as_view(), name="user"),
-    path("change-password/", ChangePasswordView.as_view(), name="change-password"),
-    path("password_reset/", include("django_rest_passwordreset.urls", namespace="password_reset")),
-    path("activate/<uidb64>/<token>", activate, name="activate"),
-    path("get-users", UserList.as_view()),
-    path("get-users/<int:pk>", UserDetail.as_view()),
-    path("resend-verification-email", resend_verification_email, name="resend_verification_email"),
+    path(
+        "register/",
+        RegisterAPI.as_view(),
+        name="register",
+    ),
+    path(
+        "login/",
+        LoginAPI.as_view(),
+        name="login",
+    ),
+    path(
+        "logout/",
+        knox_views.LogoutView.as_view(),
+        name="logout",
+    ),
+    path(
+        "logout-all/",
+        knox_views.LogoutAllView.as_view(),
+        name="logoutall",
+    ),
+    path(
+        "user/",
+        UserAPI.as_view(),
+        name="user",
+    ),
+    path(
+        "change-password/",
+        ChangePasswordView.as_view(),
+        name="change-password",
+    ),
+    path(
+        "password_reset/",
+        include(
+            "django_rest_passwordreset.urls",
+            namespace="password_reset",
+        ),
+    ),
+    path(
+        "activate/<uidb64>/<token>/",
+        activate,
+        name="activate",
+    ),
+    path(
+        "user-list/",
+        UserList.as_view(),
+    ),
+    path(
+        "user-detail/<int:pk>/",
+        UserDetail.as_view(),
+    ),
+    path(
+        "resend-verification-email/",
+        resend_verification_email,
+        name="resend_verification_email",
+    ),
+    path(
+        "address-list/",
+        AddressList.as_view(),
+    ),
+    path(
+        "address-detail/<int:pk>/",
+        AddressDetail.as_view(),
+    ),
 ]

d614c5ce5a424261982482a8f795f9b602a03b4d
arshharkial <theadsh99@gmail.com>
2022-09-11 16:21:49 +0530

    Add product tables

--- backend/market/product/models.py
+++ backend/market/product/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from brand.models import Brand
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+User = get_user_model()
+PRODUCT_CATEGORIES = []
+
+
+class Product(models.Model):
+    seller = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="my_products"
+    )
+    category = models.CharField(
+        max_length=100, blank=False, null=False, choices=PRODUCT_CATEGORIES
+    )
+    name = models.CharField(max_length=100, blank=False, null=False)
+    brand_name = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, related_name="my_brand_products"
+    )
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=False, null=False
+    )
+    images = ArrayField(
+        models.CharField(
+            null=True,
+            blank=True,
+            max_length=256,
+        ),
+        blank=True,
+        default=list,
+        null=True,
+    )
+    videos = ArrayField(
+        models.CharField(
+            null=True,
+            blank=True,
+            max_length=256,
+        ),
+        blank=True,
+        default=list,
+        null=True,
+    )
+
+
+class Reviews(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="my_reviews"
+    )
+    title = models.CharField(max_length=300, blank=False, null=False)
+    description = models.TextField()
+    reviewer = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="my_reviews"
+    )
+    rating = models.DecimalField(
+        max_digits=3, decimal_places=2, blank=False, null=False
+    )
+    images = ArrayField(
+        models.CharField(
+            null=True,
+            blank=True,
+            max_length=256,
+        ),
+        blank=True,
+        default=list,
+        null=True,
+    )
+    videos = ArrayField(
+        models.CharField(
+            null=True,
+            blank=True,
+            max_length=256,
+        ),
+        blank=True,
+        default=list,
+        null=True,
+    )

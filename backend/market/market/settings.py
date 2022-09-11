42fcf5c842ad6843cc8e37a4716c0c50d6e7bf5b
arshharkial <theadsh99@gmail.com>
2022-09-11 10:40:08 +0530

    updated fallback db name

--- backend/market/market/settings.py
+++ backend/market/market/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "market.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_NAME", "creator360_invoice"),
+        "NAME": os.environ.get("POSTGRES_NAME", "market"),
         "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
         "USER": os.environ.get("POSTGRES_USER", "postgres"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
